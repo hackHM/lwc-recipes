50b4e5915b70b4a78d6dfa0f8715b3fe268a5120
vinhnc5 <Metlife1>
2023-12-07 10:49:21 +0700

    datnd15 commit

--- jest.config.js
+++ jest.config.js
@@ -7,25 +7,29 @@ module.exports = {
         /* CSS library import fix in test context. See:
         https://github.com/salesforce/sfdx-lwc-jest/issues/288) */
         '^c/cssLibrary$':
-            '<rootDir>/force-app/main/default/lwc/cssLibrary/cssLibrary.css',
+            '/force-app/main/default/lwc/cssLibrary/cssLibrary.css',
         // Jest mocks
-        '^@salesforce/apex$': '<rootDir>/force-app/test/jest-mocks/apex',
-        '^@salesforce/schema$': '<rootDir>/force-app/test/jest-mocks/schema',
+        '^@salesforce/apex$': '/force-app/test/jest-mocks/apex',
+        '^@salesforce/schema$': '/force-app/test/jest-mocks/schema',
         '^lightning/navigation$':
-            '<rootDir>/force-app/test/jest-mocks/lightning/navigation',
+            '/force-app/test/jest-mocks/lightning/navigation',
         '^lightning/platformShowToastEvent$':
-            '<rootDir>/force-app/test/jest-mocks/lightning/platformShowToastEvent',
+            '/force-app/test/jest-mocks/lightning/platformShowToastEvent',
         '^lightning/uiRecordApi$':
-            '<rootDir>/force-app/test/jest-mocks/lightning/uiRecordApi',
+            '/force-app/test/jest-mocks/lightning/uiRecordApi',
         '^lightning/messageService$':
-            '<rootDir>/force-app/test/jest-mocks/lightning/messageService',
+            '/force-app/test/jest-mocks/lightning/messageService',
         '^lightning/actions$':
-            '<rootDir>/force-app/test/jest-mocks/lightning/actions',
-        '^lightning/modal$':
-            '<rootDir>/force-app/test/jest-mocks/lightning/modal',
-        '^lightning/refresh$':
-            '<rootDir>/force-app/test/jest-mocks/lightning/refresh'
-    },
+            '/force-app/test/jest-mocks/lightning/actions',
+        '^lightning/alert$':
+            '/force-app/test/jest-mocks/lightning/alert',
+        '^lightning/confirm$':
+            '/force-app/test/jest-mocks/lightning/confirm',
+        '^lightning/prompt$':
+            '/force-app/test/jest-mocks/lightning/prompt',
+        '^lightning/modal*':
+            '/force-app/test/jest-mocks/lightning/modal'
+      },
     setupFiles: ['jest-canvas-mock'],
     setupFilesAfterEnv,
     testTimeout: 10000
